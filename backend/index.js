90e9312e01d279d654c8c6f4f8e21044bfd491f5
dbadhikari <dipuadhikari1819@gmail.com>
2026-05-03 22:11:28 +0545

    updated

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 const frontend = {
   origin: [
     "http://localhost:5173",
-    "https://gravitydesignstudio001.netlify.app/"
+    "https://gravitydesignstudio001.netlify.app"
   ],
   credentials: true,
 };
